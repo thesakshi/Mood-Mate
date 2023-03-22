8a423ddd82849e4ec4017528686920f607d26d71
Nikesh <nikeshpoudel109@gmail.com>
2023-03-21 17:00:19 -0700

    brand new cloned directory edited

--- client/src/screens/RegistrationScreen/RegistrationScreen.js
+++ client/src/screens/RegistrationScreen/RegistrationScreen.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import { View, TextInput, Text, StyleSheet, Button, Alert } from "react-native";
-import { firebase } from "../../firebase/config";
+import { View, TextInput, StyleSheet, Button } from "react-native";
+import firebase from "firebase";
 
 const RegistrationScreen = ({ navigation }) => {
   const [email, setEmail] = useState("");
@@ -8,29 +8,27 @@ const RegistrationScreen = ({ navigation }) => {
   const [name, setName] = useState("");
 
   const handleRegistration = () => {
-    // alert(firebase);
     firebase
       .auth()
       .createUserWithEmailAndPassword(email, password)
       .then((userCredential) => {
         const user = userCredential.user;
-        // firebase.database().ref(`users/${user.uid}`).set({
-        //   email,
-        //   name,
-        // });
-        Alert.alert("Success", "Successfully Registered!", [
-          {
-            text: "OK",
-            onPress: () => {
-              // Navigate to the Login screen
-              navigation.navigate("Login");
-            },
-          },
-        ]);
+
+        firebase.database().ref(`users/${user.uid}`).set({
+          email,
+          name,
+        });
+        // notify user of successful registration
+        alert("Registration successful!");
+
+        // navigate to LoginScreen after successful registration
+        navigation.navigate("Login");
       })
       .catch((error) => {
         const errorMessage = error.message;
-        alert(errorMessage);
+        console.log(errorMessage);
+        alert("Registration failed!");
+        navigation.navigate("Registration");
       });
   };
 
@@ -41,14 +39,12 @@ const RegistrationScreen = ({ navigation }) => {
         value={name}
         placeholder="Name"
         onChangeText={setName}
-        placeholderTextColor="gray"
       />
       <TextInput
         style={styles.input}
         value={email}
         placeholder="Email"
         onChangeText={setEmail}
-        placeholderTextColor="gray"
       />
       <TextInput
         style={styles.input}
@@ -56,18 +52,8 @@ const RegistrationScreen = ({ navigation }) => {
         placeholder="Password"
         secureTextEntry
         onChangeText={setPassword}
-        placeholderTextColor="gray"
       />
       <Button title="Register" onPress={handleRegistration} />
-      <Text style={styles.loginText}>
-        Already have an account?{" "}
-        <Text
-          style={styles.loginButton}
-          onPress={() => navigation.navigate("Login")}
-        >
-          Login
-        </Text>
-      </Text>
     </View>
   );
 };
@@ -75,25 +61,17 @@ const RegistrationScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#FFF9C4",
     alignItems: "center",
     justifyContent: "center",
   },
   input: {
     width: "80%",
-    height: 40,
-    margin: 5,
+    height: 50,
+    margin: 10,
     padding: 10,
     borderWidth: 1,
     borderColor: "#ccc",
     borderRadius: 5,
-  },
-  loginText: {
-    marginTop: 20,
-    fontSize: 16,
-  },
-  loginButton: {
-    fontWeight: "bold",
   },
 });
 
